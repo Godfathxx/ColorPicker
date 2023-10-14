099820772817a6aa9ebb92e09d04985b3d1b1b8a
Shreyash Joshi <90604516+Godfathxx@users.noreply.github.com>
2023-10-14 20:45:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const ColorPicker = () => {
+  const colorOptions = [
+    'red',
+    'blue',
+    'green',
+    'yellow',
+    'orange',
+    'purple',
+    'pink',
+    'brown',
+    'cyan',
+    'magenta',
+    'lime',
+    'teal',
+    'indigo',
+    'gray',
+    'black',
+  ];
+
+  const [selectedColor, setSelectedColor] = useState(null);
+
+  const handleColorPick = (color) => {
+    setSelectedColor(color);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div
+          className="selected-color"
+          style={{
+            backgroundColor: selectedColor || 'white',
+            width: '100%',
+            height: '100%',
+            margin: '0 auto',
+            borderRadius: '25px',
+          }}
         >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: 'center' }}>
+      <h1>Color Picker</h1>
+      <div>
+        <button onClick={() => handleColorPick(colorOptions[Math.floor(Math.random() * colorOptions.length)])}>
+          Pick a Color
+        </button>
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap' }}>
+        {colorOptions.map((color, index) => (
+          <div
+            key={index}
+            onClick={() => handleColorPick(color)}
+            className="color-option"
+            style={{
+              backgroundColor: color,
+              width: '50px',
+              height: '50px',
+              margin: '10px',
+              cursor: 'pointer',
+              border:'5px',
+              borderRadius:'1px',
+              borderColor: 'black',
+            }}
+          ></div>
+        ))}
+      </div>
+    </div>
     </div>
   );
-}
+};
 
-export default App;
+export default ColorPicker;
